8335c662e71b60966b34604fd7199c2ba2cdb508
laireyx <laireyx@gmail.com>
2023-10-18 01:52:45 +0900

    ci: change base path for the GH pages hosting

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), vanillaExtractPlugin()],
 
+  base: './',
+
   build: {
     rollupOptions: {
       output: {
